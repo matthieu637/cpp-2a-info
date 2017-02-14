d76cb5c597ce64b82a00d7a58f8217e2a2bd114f
Matthieu Zimmer <contact@matthieu-zimmer.net>
2017-02-14 10:12:21 +0100

    fix windows problem

--- 2017/jeu.py
+++ 2017/jeu.py
@@ -19,7 +19,7 @@ class Reseau:
 
 	def creerPartie(self, nom):
 		self.__estConnect()
-		sent = self.sock.send("CREATE "+nom+"\n".encode())
+		sent = self.sock.send(("CREATE "+nom+"\n").encode())
 		if sent == 0:
 			raise RuntimeError("Erreur deconnexion. _1")
 		id_partie = self.sock.recv(256)
@@ -32,14 +32,14 @@ class Reseau:
 	def top(self):
 		if(not self.connect):
 			raise RuntimeError("Vous n'etes pas encore connecte.")
-		self.sock.send("TOP\n".encode())
+		self.sock.send(("TOP\n").encode())
 		r = int(self.sock.recv(256).decode())
 		self.topbool= True
 		return r
 	
 	def rejoindrePartie(self, id_partie, nom):
 		self.__estConnect()
-		sent = self.sock.send("JOIN "+str(id_partie)+" "+nom+"\n".encode())
+		sent = self.sock.send(("JOIN "+str(id_partie)+" "+nom+"\n").encode())
 		if sent == 0:
 			raise RuntimeError("Erreur deconnexion. _3")
 		ok = self.sock.recv(256)
@@ -57,47 +57,47 @@ class Reseau:
 
 	def solde(self):
 		self.__estTop()
-		self.sock.send("SOLDE\n".encode())
+		self.sock.send(("SOLDE\n").encode())
 		return eval(self.sock.recv(256).decode())
 	
 	def operationsEnCours(self):
 		self.__estTop()
-		self.sock.send("OPERATIONS\n".encode())
+		self.sock.send(("OPERATIONS\n").encode())
 		return eval(self.sock.recv(256).decode())
 
 	def ask(self, action, prix, volum):
 		self.__estTop()
-		self.sock.send("ASK "+action+" "+str(prix)+" "+str(volum)+"\n".encode())
+		self.sock.send(("ASK "+action+" "+str(prix)+" "+str(volum)+"\n").encode())
 		return int(self.sock.recv(256).decode())
 
 	def bid(self, action, prix, volum):
 		self.__estTop()
-		self.sock.send("BID "+action+" "+str(prix)+" "+str(volum)+"\n".encode())
+		self.sock.send(("BID "+action+" "+str(prix)+" "+str(volum)+"\n").encode())
 		return int(self.sock.recv(256).decode())
 
 	def achats(self, action):
 		self.__estTop()
-		self.sock.send("ACHATS "+action+"\n".encode())
+		self.sock.send(("ACHATS "+action+"\n").encode())
 		return eval(self.sock.recv(256).decode())
 	
 	def ventes(self, action):
 		self.__estTop()
-		self.sock.send("VENTES "+action+"\n".encode())
+		self.sock.send(("VENTES "+action+"\n").encode())
 		return eval(self.sock.recv(256).decode())
 
 	def historiques(self, action):
 		self.__estTop()
-		self.sock.send("HISTO "+action+"\n".encode())
+		self.sock.send(("HISTO "+action+"\n").encode())
 		return eval(self.sock.recv(256).decode())
 
 	def suivreOperation(self, id_partie):
 		self.__estTop()
-		self.sock.send("SUIVRE "+str(id_partie)+"\n".encode())
+		self.sock.send(("SUIVRE "+str(id_partie)+"\n").encode())
 		return eval(self.sock.recv(256).decode())
 
 	def annulerOperation(self, id_partie):
 		self.__estTop()
-		self.sock.send("ANNULER "+str(id_partie)+"\n".encode())
+		self.sock.send(("ANNULER "+str(id_partie)+"\n").encode())
 		return eval(self.sock.recv(256).decode())
 
 
