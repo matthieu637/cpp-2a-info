0788035db21e0d3b2a96879ef07ae91526f7c265
Matthieu Zimmer <contact@matthieu-zimmer.net>
2017-02-16 13:44:24 +0100

    nouveau classement des joueurs

--- 2017/SimBourse/src/core/Joueur.java
+++ 2017/SimBourse/src/core/Joueur.java
@@ -1,6 +1,5 @@
 package core;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -73,26 +72,51 @@ public class Joueur implements Comparable<Joueur> {
 
 		return nb_action_type;
 	}
+	
+	private int nb_action_total() {
+		int nb_action_tot = 0;
+		for (Action a : Action.values())
+				nb_action_tot+=solde_actions.get(a);
+
+		return nb_action_tot;
+	}
 
 	@Override
 	public int compareTo(Joueur o) {
 		int j1_nb_ac = nb_action_unique();
 		int j2_nb_ac = o.nb_action_unique();
+		
+		int j1_tot_ac = nb_action_total();
+		int j2_tot_ac = o.nb_action_total();
+		
 
-		if (j1_nb_ac == 1 && j2_nb_ac == 1) {// cas normal
-			int j1_max_ac = Collections.max(solde_actions.values());
-			int j2_max_ac = Collections.max(o.solde_actions.values());
-			return -Integer.compare(j1_max_ac, j2_max_ac);
-		} else if (j1_nb_ac > 0 && j2_nb_ac == 0)// j1 gagne car j2 n'a plus que de l'argent
+		if (j1_nb_ac == 2 && j2_nb_ac == 2) {// meilleur cas
+			int cmp = -Integer.compare(j1_tot_ac, j2_tot_ac);
+			if(cmp == 0)
+				return -Integer.compare(this.solde_euros, o.solde_euros);
+			return cmp;
+		} 
+		
+		if (j1_nb_ac == 2 && j2_nb_ac != 2)// j1 gagne car il a vendu les 2 types
 			return -1;
-		else if (j2_nb_ac > 0 && j1_nb_ac == 0)// j2 gagne car j1 n'a plus que de l'argent
+		
+		if (j2_nb_ac == 2 && j1_nb_ac != 2)// j2 gagne car il a vendu les 2 types
 			return 1;
-		else if (j2_nb_ac == 0 && j1_nb_ac == 0)// les 2 n'ont que de l'argent
+		
+		if (j2_nb_ac == 0 && j1_nb_ac == 0)// les 2 n'ont que de l'argent
 			return -Integer.compare(this.solde_euros, o.solde_euros);
-		else if (j1_nb_ac > 0 && j2_nb_ac > 0){ // les 2 ont plusieurs actions
-			int j1_max_ac = Collections.max(solde_actions.values());
-			int j2_max_ac = Collections.max(o.solde_actions.values());
-			return -Integer.compare(j1_max_ac, j2_max_ac);
+		
+		if (j1_nb_ac != 4 && j2_nb_ac == 4)// j1 gagne car il a vendu au moins un type d action
+			return -1;
+		
+		if (j1_nb_ac == 4 && j2_nb_ac != 4)// j2 gagne car il a vendu au moins un type d action
+			return 1;
+		
+		if (j1_nb_ac > 0 && j2_nb_ac > 0){ // les 2 ont plusieurs actions (1/3/4)
+			int cmp = -Integer.compare(j1_tot_ac, j2_tot_ac);
+			if(cmp == 0)
+				return -Integer.compare(this.solde_euros, o.solde_euros);
+			return cmp;
 		}
 		
 		System.out.println("cas non traite "+solde_actions+" "+o.solde_actions);
