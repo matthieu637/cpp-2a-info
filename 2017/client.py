b6f177eb894b4568278ef9eb35ac8f6939c34307
Matthieu Zimmer <contact@matthieu-zimmer.net>
2017-02-22 13:51:47 +0100

    ajustements doc

--- 2017/client.py
+++ 2017/client.py
@@ -8,7 +8,7 @@ class Reseau:
 	'''
 	Fait le lien entre votre programme et le serveur.
 	
-	Cette documentation a pour but de vous aider à utiliser la classe Reseau (qui fait le lien entre votre programme et le serveur) développée par Matthieu Zimmer pour la simulation de bourse du S4. En POO (programmation orientée objet), une classe est un ensemble de fonctions prédéfinies. Dans notre cas, cette classe permet de communiquer avec le serveur “boursier” écrit en Java afin d’acheter, de vendre ...
+	Cette documentation a pour but de vous aider à utiliser la classe Reseau (qui fait le lien entre votre programme et le serveur). En POO (programmation orientée objet), une classe est un ensemble de fonctions prédéfinies. Dans notre cas, cette classe permet de communiquer avec le serveur “boursier” écrit en Java afin d’acheter, de vendre ...
 	Pour plus d’informations:
 	
 	U{https://openclassrooms.com/courses/apprenez-a-programmer-en-python/premiere-approche-des-classes}
