43040252a4e4d26c5baeeda2d68e19620edb5f03
david540 <david.desobry@orange.fr>
2017-02-22 15:02:01 +0100

    Update Action.java

--- 2017/SimBourse/src/core/Action.java
+++ 2017/SimBourse/src/core/Action.java
@@ -1,14 +1,22 @@
 package core;
+import java.util.Arrays;
+
 
 public enum Action {
 	Apple("Apple"), Facebook("Facebook"), Trydea("Trydea"), Google("Google");
 
 	private String name;
+	
 
 	private Action(String name) {
 		this.name = name;
 	}
-
+	private static int nombreActions(){
+		int compteur=0;
+		for (Action a : Action.values())
+			compteur++;
+		return compteur;
+	}
 	public static boolean estValide(String s) {
 		for(Action a : Action.values())
 			if(a.name.equalsIgnoreCase(s))
@@ -22,5 +30,15 @@ public enum Action {
 				return a;
 		System.out.println("ERROR Action.from " + s);
 		return null;
+	}
+	public static String[] nomActions() {
+		String[] vectName= new String[nombreActions()];
+		int compteur=0;
+		for(Action a : Action.values()){
+			vectName[compteur]=a.name;
+			compteur++;
+		}
+		Arrays.sort(vectName);
+		return vectName;
 	}
 }
