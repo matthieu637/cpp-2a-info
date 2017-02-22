0a69ffc2763c15407992f63df1bd9f3bbefec548
david540 <david.desobry@orange.fr>
2017-02-22 15:00:55 +0100

    Update client.py

--- 2017/client.py
+++ 2017/client.py
@@ -8,65 +8,78 @@ import time
 
 class Reseau:
 	'''
-	1. Introduction
+	Fait le lien entre votre programme et le serveur.
 	
-	Cette documentation a pour but de vous aidez à utiliser la class Reseau (qui fait le lien entre votre programme et le serveur) développé par Matthieu Zimmer pour la simulation de bourse du S4. En POO (programmation orientée objet), une class est un ensemble de fonctions prédéfinies. Dans notre cas, cette class permettre de communiquer avec le serveur “boursier” écrit en Java afin d’acheter, de vendre ...
+	Cette documentation a pour but de vous aider à utiliser la classe Reseau (qui fait le lien entre votre programme et le serveur). En POO (programmation orientée objet), une classe est un ensemble de fonctions prédéfinies. Dans notre cas, cette classe permet de communiquer avec le serveur “boursier” écrit en Java afin d’acheter, de vendre ...
 	Pour plus d’informations:
 	
 	U{https://openclassrooms.com/courses/apprenez-a-programmer-en-python/premiere-approche-des-classes}
 
-	2. Préliminaires
+	Préliminaires
+	=============
 	
-	Avant de pouvoir rejoindre ou créer une partie, il faut tout d’abords importer la class (après l’avoir télécharger). Pour cela, il faut ce fichier dans le MEME dossier que le fichier courant puis dans le fichier courant on commence par:
-		>>> from client import Reseau
-		r=Reseau() #On travaillera par la suite avec la variable r
-	
-	B{Attention: Le fichier client.py ne doit JAMAIS être modifié !}
-	
-	3. Création d’une partie
-	
-	Une fois que la connexion est effectuée, on doit pouvoir choisir entre créer une partie ou en rejoindre une (à faire vous même...).
-	Pour créer une partie:
-		>>> num_partie=r.creerPartie("monNom") #On crée la partie
-		print(num_partie) #On récupère son numéro
-	
-	On récupère le numéro afin de le transmettre oralement aux autres joueurs qui eux devront rejoindre la partie.
-
-	B{Attention: Celui qui crée la partie n’a pas besoin de la rejoindre, il est directement considérer comme un joueur}
-	
-	4. Rejoindre la partie
-	
-	Une fois la partie crée par l’un des participants, les autres doivent pouvoir la rejoindre:
-	Premièrement, on rejoint la partie, en indiquant son numéro et le pseudo que l’on souhaite, par exemple,
-
-	>>> r.rejoindrePartie(4488,MatthieuDevallé)
-
-	Puis on doit se synchroniser avec les autres joueurs:
-
-	>>> r.top()
-
-	On a donc le code suivant pour rejoindre une partie:
-		>>> r.rejoindrePartie(numéro,"pseudo")
-		r.top()
+		Avant de pouvoir rejoindre ou créer une partie, il faut tout d’abord importer la class (après l’avoir 
+		U{téléchargée<https://raw.githubusercontent.com/matthieu637/cpp-2a-info/master/2017/client.py>} : clic droit, enregistré le lien sous). 
+		Pour cela, il faut metre ce fichier dans le même dossier que le fichier courant puis dans le fichier courant, on commence par:
+			>>> from client import Reseau
+			r=Reseau() #On travaillera par la suite avec la variable r
 		
-	B{Attention: On ne reprends pas la main tant que le créateur n’a pas lui même taper C{r.top}()}
+		B{Attention: Le fichier client.py ne doit JAMAIS être modifié !}
 	
-	5. Lancement de la partie
+	Création d’une partie
+	=====================
 	
-	Une fois que tous les joueurs ont rejoint, le créateur entre à son tour qui lance la partie:
+		Une fois que la connexion est effectuée, on doit pouvoir choisir entre créer une partie ou en rejoindre une (à faire vous même...).
+		Pour créer une partie:
+			>>> num_partie=r.creerPartie("monNom") #On crée la partie
+			print(num_partie) #On récupère son numéro
+		
+		On récupère le numéro afin de le transmettre oralement aux autres joueurs qui eux devront rejoindre la partie.
 	
-	>>> r.top() #Libère les autres joueurs
+		B{Attention: Celui qui crée la partie n’a pas besoin de la rejoindre, il est directement considéré comme un joueur}
+		
+	Rejoindre la partie
+	===================
+		
+		Une fois la partie créée par l’un des participants, les autres doivent pouvoir la rejoindre:
+		Premièrement, on rejoint la partie, en indiquant son numéro et le pseudo que l’on souhaite, par exemple,
+	
+		>>> r.rejoindrePartie(4488,'MatthieuDevallé')
+	
+		Puis on doit se synchroniser avec les autres joueurs:
+	
+		>>> r.top()
+	
+		On a donc le code suivant pour rejoindre une partie:
+			>>> r.rejoindrePartie(numéro_partie,"pseudo")
+			r.top()
+			
+		B{Attention: On ne reprend pas la main tant que le créateur n’a pas lui même tapé C{r.top}()}
+	
+	Synchronisation pour le départ
+	==============================
 
-	A partir de ce moment, il n’y a plus de différence entre le créateur et les autres.
+		Lorsque tous les utilisateurs ont rejoint la partie, il faut se synchroniser pour le top depart.
+		Pour cela, ceux qui ont rejoint la partie utiliseront :
+			>>> r.top() #ne rend pas la main tant que le createur n'a pas lance
+		
+		Une fois que tous les joueurs ont rejoint la partie, le créateur entre à son tour qui lance la partie:
+		
+		A partir de la, createur et utilisateurs ont acces aux memes fonctions du jeu.
+			>>> r.solde()
+			{'Apple': 100, 'Facebook': 100, 'Google': 100, 'Trydea': 100, 'euros': 1000}
 	
-	6. Informations sur le classement final
+		A partir de ce moment, il n’y a plus de différence entre le créateur et les autres.
+		
+	Informations sur le classement final
+	====================================
 		
 		Il y a plusieurs cas (on note j1 et j2 les joueurs):
 			- dans le cas où j1 (ou j2) a vendu les deux types d'actions, il gagne
 			- dans le cas où j1 (ou j2) a vendu au moins un type d'action et pas l'autre, il gagne
 			- dans le cas où les deux n'ont que de l'argent, le plus riche gagne
-			- dans le cas où les deux ont plusieurs actions, on compare leurs nombre d'action et leur argent, le plus riche en action gagne sauf si ils ont le même nombre d'actions (le plus riche gagne)
-			- dans le meilleur cas, si ils ont vendus deux types d'actions, on compare leur nombre d'action ainsi que leur argent, le plus riche gagne
+			- dans le cas où les deux ont plusieurs actions, on compare leur nombre d'action et leur argent, le plus riche en action gagne sauf si ils ont le même nombre d'actions (le plus riche gagne)
+			- dans le meilleur cas, s'ils ont vendu deux types d'actions, on compare leur nombre d'action ainsi que leur argent, le plus riche gagne
 			
 	'''
 	
@@ -76,6 +89,7 @@ class Reseau:
 	def __init__(self, host="matthieu-zimmer.net", port=23456):
 		self.message={"TOP":"1","SOLDE":"2","OPERATIONS":"3","ACHATS":"4 ","VENTES":"5 ","HISTO":"6 ","ASK":"7 ","BID":"8 ",
 		"SUIVRE":"9 ","ANNULER":"A ","FIN":"B","CREATE":"C ","JOIN":"D "}
+		self.nomAction=[]
 		self.connect = False
 		self.topbool = False
 		self.histoActions={}
@@ -121,20 +135,29 @@ class Reseau:
 		except (ConnectionRefusedError):
 			raise RuntimeError("Connexion perdu. _4")
 			
-
+	def __chercherNumAction(self,action):
+		numAction=-1
+		#recherche du numero de l'action (triee dans l'ordre alphabetique)
+		for i in range(len(self.nomAction)):
+			if action==self.nomAction[i]:
+				numAction =i 
+				break
+		return numAction
+		
 	def creerPartie(self, nom):
 		'''
-		Crée la partie et renvoie l’id a communiqué oralement aux autres joueurs.
+		Crée la partie et renvoie l’id à communiqur oralement aux autres joueurs.
+		Exemple:
+
+		>>> id=r.creerPartie("MatthieuDevallé")
+		>>> print(id)
+		31416 #id de la partie
 		
-		@param nom: nom du joueur qui crée la partie
+		@param nom: le nom du joueur qui cree la partie
 		@type nom: string
 		
 		
-		Exemple:
-
-			>>> id=r.creerPartie("MatthieuDevallé")
-			>>> print(id)
-			31416 #id de la partie
+	
 		'''
 		self.__estConnect()
 		self.__envoyer(self.message["CREATE"]+nom)
@@ -144,23 +167,16 @@ class Reseau:
 	
 	def rejoindrePartie(self, id_partie, nom):
 		'''
-		Permet de rejoindre la partie.
-		
 		Renvoie : 
-			- 0 si tout c'est bien passé.
+			- 0 si tout s'est bien passé.
 			- -1 si le numero de partie n'existe pas
 			- -2 si le nom de joueur est déja pris
-			- -3 si la partie est déja lancé (top)
+			- -3 si la partie est déja lancée (top)
 			- -4 si les types ne sont pas respectés
 		
-		Exemple:
-
-			>>> r.rejoindrePartie(31416,"MatthieuDev")
-			0 #Forcement, ça marche
-			
-		@param id_partie: l'id de la partie (donné oralement par le créateur)
+		@param id_partie: le numero de la partie qui m'a ete communiquer oralement
 		@type id_partie: entier
-		@param nom: le pseudo du joueur qui rejoint la partie
+		@param nom: le nom du joueur qui rejoint la partie
 		@type nom: string
 		'''
 		self.__estConnect()
@@ -171,15 +187,9 @@ class Reseau:
 
 	def top(self):
 		'''
-		Synchronisation
+		Si vous avez rejoint, attend le top depart du createur.
 		
-		Deux possibilités:
-			- si vous avez rejoint la partie, attend le top du créateur
-			- si vous avez créé, lance la partie
-		
-		Exemple:
-
-			>>> r.top()
+		Si vous avez cree, donne le top depart aux autres.
 		
 		Renvoie toujours 0.
 		'''
@@ -193,14 +203,15 @@ class Reseau:
 
 		for key in self.solde():
 			if key!='euros':
+				self.nomAction.append(key)
 				self.histoActions[key]=[] #on ajoute les différentes actions dans le tableau historique du client
-		
+		self.nomAction.sort()
 
 		return r
 
 	def solde(self):
 		'''
-		Permet de voir notre prote-feuille d’actions et notre argent disponible
+		Permet de voir notre porte-feuille d’actions et notre argent disponible
 		Renvoie un dictionnaire (string:entier).
 		
 		Exemple:
@@ -215,12 +226,13 @@ class Reseau:
 	
 	def operationsEnCours(self):
 		'''
-		Retroune une liste d’entier, qui correspondent aux identifiants des ordres précédements transmis et qui ne sont pas encore terminer: on peut donc les suivres et les annuler.
+		Retourne une liste d’entiers, qui correspondent aux identifiants des ordres précédemment transmis et qui ne sont pas encore terminés: on peut donc les suivre et les annuler.
 
 		Exemple:
 
-		>>> r.rejoindrePartie(31416,"MatthieuDev")
-		0 #Forcement, ça marche
+		>>> R.operationsEnCours()
+		[62098581, 20555477]
+
 		'''
 		self.__estTop()
 		self.__notEnd()
@@ -229,60 +241,58 @@ class Reseau:
 
 	def ask(self, action, prix, volume):
 		'''
-		Méthode permettant de passer un ordre d’achat.
-
-		Retourne:
-
-			- 0 si l’ordre a été exécuté directement et que tout son volume a été écoulé
-			- 4 si les types ne sont pas respectés
-			- 5 si volume <= 0
-			- 6 si prix <= 0
-			- 7 si vous n’avez pas assez d’argent pour acheter cette quantité (prix*volume)
-			- sinon renvoie l’identifiant de l’ordre (nombre positif)
-	
-		Exemple:
-	
-			>>> r.ask('Trydea',500,30) #On veut acheter 30 actions de Trydea à un prix unitaire de 500 euros
+		Passer un ordre d'achat.
+		Renvoie :
+			- 0 si l'ordre a ete execute directement et que tout son volume a ete ecoule
+			- -4 si les types ne sont pas respectes
+			- -5 si volume <= 0
+			- -6 si prix <= 0
+			- -7 si vous n'avez pas assez d'argent pour acheter cette quantite (prix*volume)
+			- sinon renvoie l'identifiant de l'ordre (nombre positif)
 		
 		@param action: le nom de l'action 
 		@type action: string
 		@param prix: prix unitaire d'achat
 		@type prix: flottant
-		@param volume: nombre d’actions à acheter
+		@param volume: le nombre d'action que vous voulez acheter
 		@type volume: entier
 		'''
 		self.__estTop()
 		self.__notEnd()
-		self.__envoyer(self.message["ASK"]+action+" "+str(prix)+" "+str(volume))
+		#recherche du numero de l'action (triee dans l'ordre alphabetique)
+		numAction=self.__chercherNumAction(action)
+		if numAction==-1: #si le nom de l'action n'est pas valide on retourne -4
+			return -4
+		#on envoie le numero de l'action
+		self.__envoyer(self.message["ASK"]+str(numAction)+" "+str(prix)+" "+str(volume))
 		return eval(self.__recevoir())
 
 	def bid(self, action, prix, volume):
 		'''
-		Permet de passer un ordre de vente.
-
-		Retourne:
-			- 0 si l’ordre a été executé directement et que tout son volume a été écoulé
-			- 4 si les types ne sont pas respectés
-			- 8 si volume <= 0
-			- 9 si prix <= 0
-			- 10 si vous n’avez pas assez d’action de ce type dans votre portefeuille
-			- sinon renvoie l’identifiant de l’ordre (nombre positif)
+		Passer un ordre de vente.
+		Renvoie :
+			- 0 si l'ordre a ete execute directement et que tout son volume a ete ecoule
+			- -4 si les types ne sont pas respectes
+			- -8 si volume <= 0
+			- -9 si prix <= 0
+			- -10 si vous n'avez pas assez d'action de type action dans votre portefeuille
+			- sinon renvoie l'identifiant de l'ordre (nombre positif)
 		
-		Exemple:
-
-		>>> r.bid("Trydea", 50, 10)
-		0
-		
-		@param action: nom de l'action
+		@param action: le nom de l'action 
 		@type action: string
 		@param prix: prix unitaire de vente
 		@type prix: flottant
-		@param volume: volume d’action à vendre
+		@param volume: le nombre d'action que vous voulez vendre
 		@type volume: entier
 		'''
 		self.__estTop()
 		self.__notEnd()
-		self.__envoyer(self.message["BID"]+action+" "+str(prix)+" "+str(volume))
+		#recherche du numero de l'action (triee dans l'ordre alphabetique)
+		numAction=self.__chercherNumAction(action)
+		if numAction==-1: #si le nom de l'action n'est pas valide on retourne -4
+			return -4
+		#on envoie le numero de l'action
+		self.__envoyer(self.message["BID"]+str(numAction)+" "+str(prix)+" "+str(volume))
 		return eval(self.__recevoir())
 
 	def achats(self, action):
@@ -290,24 +300,31 @@ class Reseau:
 		Liste tous les ordres d’achats pour tous les joueurs sur une action donnée.
 		Retourne:
 			- -4 si l’action n’existe pas
-			- une liste de tuples triée par ordre de prix avantageux sous la forme: C{nom_acheteur, prix, volume)}
+			- une liste de tuples triée par ordre de prix avantageux sous la forme: C{(nom_acheteur, prix, volume)}
 		
 		Exemple:
 
-		>>> r.ventes("Trydea")
-		[(Matthieu, 23,15), (Ryan,20,10), (Paul, 17,23)]
+		>>> r.achats("Trydea")
+		[('Matthieu', 23,15), ('Ryan',20,10), ('Paul', 17,23)]
 		
-		@param action: nom de l'action
+		@param action: le nom de l'action pour laquelle vous voulez voir les offres d'achats
 		@type action: string
 		'''
 		self.__estTop()
 		self.__notEnd()
-		self.__envoyer(self.message["ACHATS"]+action)
+		#recherche du numero de l'action (triee dans l'ordre alphabetique)
+		numAction=self.__chercherNumAction(action)
+		if numAction==-1: #si le nom de l'action n'est pas valide on retourne -4
+			return -4
+		#on envoie le numero de l'action
+		self.__envoyer(self.message["ACHATS"]+str(numAction))
 		return eval(self.__recevoir())
 	
 	def ventes(self, action):
 		'''
-		Liste tous les ordres de ventes pour tous les joueurs sur une action donnée.
+		Liste tous les ordres de ventes ouverts de tous les utilisateurs pour une action donnee.
+		Renvoie une liste de tuple (nom_acheteur, prix, volume) triee par le prix le plus avantageux.
+		Si l'action n'existe pas renvoie -4;
 		
 		Retourne:
 			- -4 si l’action n’existe pas
@@ -315,52 +332,59 @@ class Reseau:
 	
 		Exemple:
 
-		>>> r.bid("Trydea", 50, 10)
-		0
+		>>> r.ventes('Facebook')
+		[('Matthieu', 5.0, 5), ('banque', 25.0, 40000)]
+
+		
 
 		@param action: nom de l'action
 		@type action: string
 		'''
 		self.__estTop()
 		self.__notEnd()
-		self.__envoyer(self.message["VENTES"]+action)
+		#recherche du numero de l'action (triee dans l'ordre alphabetique)
+		numAction=self.__chercherNumAction(action)
+		if numAction==-1: #si le nom de l'action n'est pas valide on retourne -4
+			return -4
+		#on envoie le numero de l'action
+		self.__envoyer(self.message["VENTES"]+str(numAction))
 		return eval(self.__recevoir())
 
 	def historiques(self, action):
 		'''
-		Permet de lister tous les échanges déjà effectuer sur une actions.
-		Retourne une liste de tuples trier par ordre chronologique. Sous la forme: C{(nom_vendeur, nom_acheteur, prix, volume)}
+		Permet de lister tous les échanges déjà effectués sur une action.
+		Retourne une liste de tuples triée par ordre chronologique. Sous la forme: C{(nom_vendeur, nom_acheteur, prix, volume)}
 		
 		Exemple:
 
 		>>> r.historiques("Trydea")
-		[(Matthieu,Mukhlis,10,10), (Térence, Ryan, 15,20), (Matthieu, Ryan, 20,3)]
+		[('Matthieu','Mukhlis',10,10), ('Térence', 'Ryan', 15,20), ('Matthieu', 'Ryan', 20,3)]
 		
-		@param action: nom de l'action
+		@param action: le nom de l'action
 		@type action: string
 		'''
 		self.__estTop()
 		self.__notEnd()
-		self.__envoyer(self.message["HISTO"]+action+" "+str(len(self.histoActions[action])))
+		#recherche du numero de l'action (triee dans l'ordre alphabetique)
+		numAction=self.__chercherNumAction(action)
+		if numAction==-1: #si le nom de l'action n'est pas valide on retourne -4
+			return -4
+		#on envoie le numero de l'action
+		self.__envoyer(self.message["HISTO"]+str(numAction)+" "+str(len(self.histoActions[action])))
 		self.histoActions[action]+=(eval(self.__recevoir()))
 		return self.histoActions[action]
 		#return eval(self.__recevoir())
 
 	def suivreOperation(self, id_ordre):
 		'''
-		Permet de voir le volume restant pour un ordre transmis précédement.
+		Permet de voir le volume restant pour un ordre transmis précédemment.
 		
 		Retourne:
 			- 0 si l’ordre n’existe plus ou est terminé
 			- 4 si les types ne sont pas respectés
 			- sinon le volume restant en achat/vente.
 		
-		Exemple:
-
-		>>> r.suivreOperation(31416)
-		10
-		
-		@param id_ordre: id de l’odre (récupérer à partir de la fonction operationsEnCours())
+		@param id_ordre: idenfiant unique de l'ordre a suivre
 		@type id_ordre: entier
 		'''
 		self.__estTop()
@@ -413,4 +437,3 @@ class Reseau:
 		#si la partie est finie on fait une requete au serveur pour qu'il donne la liste des vainqueurs
 		self.__envoyer(self.message["FIN"])
 		return eval(self.__recevoir())
-
