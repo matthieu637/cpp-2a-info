83feb994eeae9925d498f2b734d4b6f7f536c5d4
Matthieu Zimmer <contact@matthieu-zimmer.net>
2017-02-26 13:56:09 +0100

    optimisation protocol réseau + correction #13

--- 2017/SimBourse/src/core/Config.java
+++ 2017/SimBourse/src/core/Config.java
@@ -8,6 +8,8 @@ public class Config {
 	public final int PORT = 23456; //23456 ou 80
 	public final int TEMPS_PARTIE = 10;//en minutes
 	public final int PACKET_SIZE = 8;
+	public final int MAX_PACKET_SIZE_INPUT = 128;
+	public final int RESERVED_SIZE_SEND_PACKET = (int) (Math.pow(10, PACKET_SIZE)-1);
 	public final boolean BANQUE = true;
 	public final String CHEMIN_FICHIER="OperationsHistorique.txt";
 	public final String VERSION = "1.0";
