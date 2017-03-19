312039720e2a3704ed873f7e79a1859d230d49b0
david540 <david.desobry@orange.fr>
2017-03-19 12:35:29 +0100

    test si joueur est déjà présent Partie.java

--- 2017/SimBourse/src/network/Partie.java
+++ 2017/SimBourse/src/network/Partie.java
@@ -1,6 +1,7 @@
 package network;
 
 import java.net.Socket;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,9 +11,13 @@ import core.Marche;
 public class Partie {
 	private final Marche marche;
 	private final List<Socket> liste_client;
-
-	public Partie() {
-		marche = new Marche();
+	private final List<String> liste_HostAdress;
+	private final int modeExam;
+	
+	public Partie(int modeBanque, int modeExamen) {
+		modeExam=modeExamen;
+		liste_HostAdress=new LinkedList<String>();
+		marche = new Marche(modeBanque);
 		liste_client = new LinkedList<>();
 	}
 
@@ -22,7 +27,25 @@ public class Partie {
 	
 	public Joueur ajouter_client(Socket s, String nom, String nom_complet){
 		liste_client.add(s);
+		if(modeExam==1)
+			liste_HostAdress.add(s.getInetAddress().getHostAddress());
 		return marche.creer_joueur(nom, nom_complet+":"+s.getInetAddress().getHostAddress());
+	}
+	
+	public Boolean isModeExamen(){
+		if(modeExam==1)
+			return true;
+		return false;
+	}
+	public Boolean testUniciteDeLaConnexion(Socket s){
+
+		String hostAdress=s.getInetAddress().getHostAddress();
+		Iterator<String> iter= liste_HostAdress.iterator();
+		while(iter.hasNext())
+			if(hostAdress.equals(iter.next()))
+				return false;
+		
+		return true;
 	}
 
 	public Marche getMarche() {
