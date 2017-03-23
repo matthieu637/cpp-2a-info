f000000e06cf2924ff72ba7b619916415ae9c91c
david540 <david.desobry@orange.fr>
2017-03-23 16:28:13 +0100

    Update Config.java

--- 2017/SimBourse/src/core/Config.java
+++ 2017/SimBourse/src/core/Config.java
@@ -22,7 +22,7 @@ public class Config {
 	public final int POW_EURO_INIT_MAX = 6;// euros max : 1 000 000
 	public final int SOLDE_ACTIONS_INIT = 1000;
 	public final int PORT = 23456; // 23456 ou 80
-	public final int TEMPS_PARTIE = 1;// en minutes
+	public final int TEMPS_PARTIE = 5;// en minutes
 	public final int PACKET_SIZE = 8;
 	public final int MAX_PACKET_SIZE_INPUT = 128;
 	public final int RESERVED_SIZE_SEND_PACKET = (int) (Math.pow(10, PACKET_SIZE) - 1);
